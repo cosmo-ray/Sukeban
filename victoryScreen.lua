dc5a05918b29341ccc637ff827b2be53a8cde444
matthias <uso.cosmo.ray@gmail.com>
2020-02-12 23:17:11 +0100

    fix look
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- victoryScreen.lua
+++ victoryScreen.lua
@@ -1,3 +1,4 @@
+local phq = Entity.wrapp(ygGet("phq"))
 
 function victoryScreenAction(vs, eve)
    eve = Event.wrapp(eve)
@@ -24,45 +25,50 @@ function autoLoot(main, pj, txt)
    return txt .. math.floor(nb) .. ": " .. "money" .. "\n"
 end
 
-function pushNewVictoryScreen(main, winner, looser)
+function pushNewVictoryScreen(main, unused, loosers)
    local victoryScreen = Entity.new_array()
    local txt = "tatatata ta ta ta tata\nloot:\n"
-   local loot = looser.loot
+   local i = 0
 
-   print(loot)
-   if loot then
-      if yeType(loot) == YSTRING then
-	 if loot:to_string() == "auto" then
-	    txt = autoLoot(main, winner, txt)
-	 elseif loot:to_string() ~= "none" then
-	    txt = txt .. "1: " .. loot:to_string() .. "\n"
-	    addObject(main, winner, loot:to_string(), 1)
-	 end
-      elseif yeType(loot) == YARRAY then
-	 local i = 0
-	 while i < yeLen(loot) do
-	    if yeGetKeyAt(loot, i) then
-	       print(yeGetKeyAt(loot, i))
-	       txt = txt .. math.floor(yeGetIntAt(loot, i)) .. ": " ..
-		  yeGetKeyAt(loot, i) .. "\n"
-	       addObject(main, winner, yeGetKeyAt(loot, i), yeGetIntAt(loot, i))
-	    else
-	       if yeGetStringAt(loot, i) == "auto" then
-		  txt = autoLoot(main, winner, txt)
-	       else
-		  txt = txt .. "1: " .. yeGetStringAt(loot, i) .. "\n"
-		  addObject(main, winner, yeGetStringAt(loot, i), 1)
-	       end
+   while i < yeLen(loosers) do
+      local loot = loosers[i].loot
+
+      print(loot)
+      if loot then
+	 if yeType(loot) == YSTRING then
+	    if loot:to_string() == "auto" then
+	       txt = autoLoot(main, phq.pj, txt)
+	    elseif loot:to_string() ~= "none" then
+	       txt = txt .. "1: " .. loot:to_string() .. "\n"
+	       addObject(main, phq.pj, loot:to_string(), 1)
 	    end
-	    i = i + 1
+	 elseif yeType(loot) == YARRAY then
+	    local i = 0
+	    while i < yeLen(loot) do
+	       if yeGetKeyAt(loot, i) then
+		  print(yeGetKeyAt(loot, i))
+		  txt = txt .. math.floor(yeGetIntAt(loot, i)) .. ": " ..
+		     yeGetKeyAt(loot, i) .. "\n"
+		  addObject(main, phq.pj, yeGetKeyAt(loot, i), yeGetIntAt(loot, i))
+	       else
+		  if yeGetStringAt(loot, i) == "auto" then
+		     txt = autoLoot(main, phq.pj, txt)
+		  else
+		     txt = txt .. "1: " .. yeGetStringAt(loot, i) .. "\n"
+		     addObject(main, phq.phq, yeGetStringAt(loot, i), 1)
+		  end
+	       end
+	       i = i + 1
+	    end
 	 end
+      else
+	 txt = autoLoot(main, phq.pj, txt)
       end
-   else
-      txt = autoLoot(main, winner, txt)
+      i = i + 1
    end
 
    local xp_win = yuiRand() % 5 + 1
-   winner.xp = winner.xp + xp_win
+   phq.pj.xp = phq.pj.xp + xp_win
    txt = txt .. "xp: " .. math.floor(xp_win) .. "\n"
    victoryScreen["<type>"] = "text-screen"
    victoryScreen["text-align"] = "center"
