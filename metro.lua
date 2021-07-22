b5b307739e360a9c6b32f2a25d94db77933429bb
matthias <uso.cosmo.ray@gmail.com>
2021-07-23 00:39:42 +0200

    small improvement on metro
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- metro.lua
+++ metro.lua
@@ -250,26 +250,30 @@ function pushMetroMenu(main)
    local can = Canvas.new_entity()
 
    can:new_img(0, 0, modPath .. "/metro.png")
-   can.ent.rect = can:new_rect(station[0] - 10, station[1] - 10,
-			       "rgba: 0 0 0 124",
-			       Size.new(20, 20).ent).ent
    can.ent.arrow = can:new_img(station[0] - 35,
 			       station[1] - 10, arrow_path,
 			       Rect.new(0, 0, 25, 20).ent).ent
+   local info_str = ""
 
-   can:new_rect(20, 550, "rgba: 0 0 0 124", Size.new(20, 20).ent)
-   can:new_text(50, 550, ": current position")
-   can:new_img(20, 520, arrow_path, Rect.new(0, 0, 25, 20).ent)
-   can:new_text(50, 520, ": destination")
+   if usable_metro == true then
+      can.ent.rect = can:new_rect(station[0] - 10, station[1] - 10,
+				  "rgba: 0 0 0 124",
+				  Size.new(20, 20).ent).ent
+      can:new_rect(20, 550, "rgba: 0 0 0 124", Size.new(20, 20).ent)
+      can:new_text(50, 550, ": current position")
+      can:new_text(50, 520, ": destination")
+   else
+      info_str = "NOT CURRENTLY IN A METRO"
+      can:new_text(50, 520, ": pointed station")
+   end
+   can.ent.info_txt = can:new_text(250, 100, info_str).ent
    can:new_text(150, 30, "Welcom to " .. phq.env.city:to_string() .. " Metro Plan")
+
    can.ent.line_txt_info = can:new_text(250, 60,
 					line_actual0 .. math.floor(l_idx)).ent
    can.ent.st_txt_info = can:new_text(250, 80, printable_st_name(station)).ent
-   local info_str = ""
-   if usable_metro == false then
-      info_str = "NOT CURRENTLY IN A METRO"
-   end
-   can.ent.info_txt = can:new_text(250, 100, info_str).ent
+
+   can:new_img(20, 520, arrow_path, Rect.new(0, 0, 25, 20).ent)
 
    can:new_text(500, 500, "left/right: change station\n\nup/down: change line")
    can.ent.action = Entity.new_func("metroAction")
