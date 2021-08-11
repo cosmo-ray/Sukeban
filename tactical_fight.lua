f81a3709725ccf42324192bb278095b3ec2a36d7
Matthias Gatto <matthias.gatto@protonmail.com>
2021-08-11 10:46:23 +0200

    button and end turn
    Signed-off-by: Matthias Gatto <matthias.gatto@protonmail.com>

--- tactical_fight.lua
+++ tactical_fight.lua
@@ -8,10 +8,21 @@ local pix_mouse_floor_left = 0
 
 local BAR_H = 100
 local bar_y = 0
+local bar_x = 0
 
 local HERO_TEAM = 0
 
 TACTICAL_FIGHT_MODE = MODE_NO_TACTICAL_FIGHT
+
+local current_character = 0
+
+local function end_tun(tdata)
+   tdata = Entity.wrapp(tdata)
+
+   print("END TURN !!!!")
+   current_character = current_character + 1
+   current_character = current_character % yeLen(tdata.all)
+end
 
 local function end_fight()
    local t = main_widget.tactical
@@ -19,6 +30,35 @@ local function end_fight()
    main_widget.tactical = nil
    TACTICAL_FIGHT_MODE = MODE_NO_TACTICAL_FIGHT
    main_widget.cam_offset = nil
+end
+
+local function push_button(tdata, rect, txt, callback)
+   local tatcical_can = tdata.screen
+   local b = Entity.new_array(tdata.buttons)
+   ywPosAdd(rect, bar_x, bar_y)
+   b[0] = rect
+   b[1] = callback
+   b[2] = ywCanvasNewRectangle(tatcical_can, ywRectX(rect), ywRectY(rect),
+			       ywRectW(rect), ywRectH(rect), "rgba: 0 255 0 200")
+   b[3] = ywCanvasNewTextByStr(tatcical_can, ywRectX(rect), ywRectY(rect), txt)
+end
+
+local function highlight_button(tdata, b)
+   if yIsNil(b[4]) then
+      local rect = b[0]
+
+      b[4] = ywCanvasNewRectangle(tdata.screen, ywRectX(rect) + 2,
+				  ywRectY(rect) + 2,
+				  ywRectW(rect) - 4, ywRectH(rect) - 4,
+				  "rgba: 0 0 0 100")
+   end
+end
+
+local function unlight_button(tdata, b)
+   if yIsNNil(b[4]) then
+      ywCanvasRemoveObj(tdata.screen, b[4])
+      b[4] = nil
+   end
 end
 
 function do_tactical_fight(eve)
@@ -36,6 +76,8 @@ function do_tactical_fight(eve)
       tdata.bads = {}
       tdata.goods = {}
       tdata.all = {}
+      tdata.buttons = {}
+      current_character = 0
 
       local pjPos = Pos.wrapp(ylpcsHandePos(main_widget.pj))
       local args = tdata.args
@@ -134,38 +176,66 @@ function do_tactical_fight(eve)
 						 "rgba: 255 255 255 80")
 
       tdata.turn_o_str = ywCanvasNewTextByStr(tdata.screen, wid_w - 100, 10, "")
+      push_button(tdata, Rect.new(ywSizeW(sz) - 160, 4, 100, 30).ent,
+		  "end turn", Entity.new_func(end_tun))
       main_widget.current = 0
+
    end -- init out
 
    local all_char = tdata.all
-   if yevIsKeyDown(eve, Y_ESC_KEY) then
-      return end_fight()
-   end
-   local mx = yeveMouseX()
-   local my = yeveMouseY()
-   local turn_timer = ywidTurnTimer() / 10000
-
-   mouse_mv = turn_timer * PIX_MV_PER_MS + pix_mouse_floor_left
-   pix_mouse_floor_left = mouse_mv - math.floor(mouse_mv)
 
 
-   if mx > wid_w - 5 then
-      yeAddAt(main_widget.cam_offset, 0, mouse_mv)
-   elseif mx < 5 then
-      yeAddAt(main_widget.cam_offset, 0, -mouse_mv)
-   end
-   if my > wid_h - 5 then
-      yeAddAt(main_widget.cam_offset, 1, mouse_mv)
-   elseif my < 5 then
-      yeAddAt(main_widget.cam_offset, 1, -mouse_mv)
-   end
+   if TACTICAL_FIGHT_MODE == MODE_PLAYER_TURN then 
+      if yevIsKeyDown(eve, Y_ESC_KEY) then
+	 return end_fight()
+      end
+      local mx = yeveMouseX()
+      local my = yeveMouseY()
+      local turn_timer = ywidTurnTimer() / 10000
+
+      mouse_mv = turn_timer * PIX_MV_PER_MS + pix_mouse_floor_left
+      pix_mouse_floor_left = mouse_mv - math.floor(mouse_mv)
+
+
+      if mx > wid_w - 5 then
+	 yeAddAt(main_widget.cam_offset, 0, mouse_mv)
+      elseif mx < 5 then
+	 yeAddAt(main_widget.cam_offset, 0, -mouse_mv)
+      end
+      if my > wid_h - 5 then
+	 yeAddAt(main_widget.cam_offset, 1, mouse_mv)
+      elseif my < 5 then
+	 yeAddAt(main_widget.cam_offset, 1, -mouse_mv)
+      end
+
+      local buttons = tdata.buttons
+      for i = 0, yeLen(buttons) - 1 do
+	 local b = buttons[i]
+	 local isOver = ywRectContain(b[0], mx, my, 1) > 0
+	 if isOver then
+	    highlight_button(tdata, b)
+	    if yevMouseDown(eve) then
+	       if yIsNNil(b[1]) then
+		  b[1](tdata)
+	       end
+	    end
+	 else
+	    unlight_button(tdata, b)
+	 end
+      end
+
+
+   end -- player turn
 
    local turn_order_str = ""
    for i = 0, yeLen(all_char) -1 do
-      turn_order_str = turn_order_str .. yeGetString(all_char[i][2]) .. "\n"
+      local idx = (i + current_character) % yeLen(all_char)
+
+      turn_order_str = turn_order_str .. yeGetString(all_char[idx][2]) .. "\n"
    end
 
    ywCanvasStringSet(tdata.turn_o_str, Entity.new_string(turn_order_str))
+
    reposeCam(main_widget)
    return YEVE_ACTION
 end
