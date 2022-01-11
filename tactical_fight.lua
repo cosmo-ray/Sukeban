b0eba0c7836c618a18a642a30bc6113ee97f0149
Matthias Gatto <matthias.gatto@protonmail.com>
2022-01-12 00:53:11 +0100

    Update tactical_fight.lua

--- tactical_fight.lua
+++ tactical_fight.lua
@@ -1,3 +1,20 @@
+--
+--Copyright (C) 2022 Matthias Gatto
+--
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Lesser General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU Lesser General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
 MODE_NO_TACTICAL_FIGHT = 0
 MODE_TACTICAL_FIGHT_INIT = 1
 local MODE_PLAYER_TURN = 2
