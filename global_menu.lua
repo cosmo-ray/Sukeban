82a7556bbaab03050f47680c2ac6b25be418e11f
Matthias Gatto <matthias.gatto@protonmail.com>
2022-01-12 00:51:55 +0100

    Update global_menu.lua

--- global_menu.lua
+++ global_menu.lua
@@ -1,3 +1,20 @@
+--
+--Copyright (C) 2022 Matthias Gatto
+--
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Lesser General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU Lesser General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
 local tiled = Entity.wrapp(ygGet("tiled"))
 local lpcs = Entity.wrapp(ygGet("lpcs"))
 local phq = Entity.wrapp(ygGet("phq"))
